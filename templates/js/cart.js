52c930066d671075870a20480b4bd1ac897d2f5c
madjid123 <dexmadjid@gmail.com>
2020-10-02 16:19:29 +0100

    fixing sidebar

--- templates/js/cart.js
+++ templates/js/cart.js
@@ -4,17 +4,22 @@ function openNav() {
 function closeNav() {
 
 }
+var SideBarShown = false;
 var openbtn = document.getElementById("openbtn1");
 var closebtn = document.getElementById("closebtn")
 openbtn.addEventListener('click', e => {
     e.preventDefault()
-    document.getElementById("mySideBar").style.width = "40%";
+    SideBarShown = !SideBarShown;
+    if (SideBarShown === true)
+        document.getElementById("mySideBar").style.width = "30%";
+    else
+        document.getElementById("mySideBar").style.width = "0";
     //document.getElementById("main").style.marginLeft = "250px";
 
 })
 closebtn.addEventListener('click', e => {
     e.preventDefault()
-    document.getElementById("mySideBar").style.width = "0";
+
     //document.getElementById("main").style.marginLeft = "0";
 
 })
