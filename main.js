a077694719bc6350efc336c295a5e3e8a993390b
madjid123 <dexmadjid@gmail.com>
2020-10-11 19:05:44 +0100

    styling the admin Board

--- main.js
+++ main.js
@@ -46,6 +46,7 @@ app.set('views', path.join(__dirname, 'templates/views'))
 var PORT = process.env.PORT || 5000
 
 var server = app.listen(PORT, () => {
+
   var host = server.address().address
   var port = server.address().port
   console.log("Example app listening at http://%s:%s", host, port)
