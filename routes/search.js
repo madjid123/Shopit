1045324f4b4bd36669e6ad0e070be6bdb39463ad
unknown <dexmadjid@gmail.com>
2020-12-08 00:14:27 +0100

    Implementing the search bar backend

--- routes/search.js
+++ routes/search.js
@@ -4,5 +4,10 @@ const { Router, db } = require("./login")
 
 
 Router.get('/search', (req, res) => {
-
+    const pattern = req.body.key
+    const SearhQuery = `SELECT * FROM Product WHERE NAME LIKE '%${pattern}%' `
+    db.all(SearhQuery, [pattern], (err, rows) => {
+        if (err) console.log(err)
+        res.render('search-res', { title: `${pattern} result`, rows: rows })
+    })
 })
