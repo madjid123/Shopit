a39e6702962f0e4aa732598650bcd0c2660e20ab
unknown <dexmadjid@gmail.com>
2020-12-08 02:07:55 +0100

    fixing the search bat

--- routes/search.js
+++ routes/search.js
@@ -1,12 +1,13 @@
-const { Router, db } = require("./login")
+const { Router, db } = require("./Router")
 
 
 
 
-Router.get('/search', (req, res) => {
+Router.post('/search', (req, res) => {
     const pattern = req.body.key
+    console.log(pattern)
     const SearhQuery = `SELECT * FROM Product WHERE NAME LIKE '%${pattern}%' `
-    db.all(SearhQuery, [pattern], (err, rows) => {
+    db.all(SearhQuery, (err, rows) => {
         if (err) console.log(err)
         res.render('search-res', { title: `${pattern} result`, rows: rows })
     })
