167d09b39fa1c879ffcd008d00b03141aae278c1
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2023-08-04 14:06:33 +0100

    redirect to login if not logged in

--- routes/explore.js
+++ routes/explore.js
@@ -1,5 +1,8 @@
 const { Router } = require('./login')
 Router.get('/explore', (req, res) => {
+    if(req.session.user === undefined){
+        return res.redirect("/log-in")
+    }
     res.render('explore', { title: " Explore", user: req.session.user, explore: 'exp' });
 }
 );
